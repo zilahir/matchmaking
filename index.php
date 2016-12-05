d809de6f2d1b26a8e8edc8815aadf0dec826f767
richard.zilahi <richard.zilahi@smartx.eu>
2016-12-05 16:31:31 +0100

    namecard display has been added.

--- index.php
+++ index.php
@@ -12,14 +12,33 @@
 	<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 	<link rel=stylesheet href="bower_components/font-awesome/css/font-awesome.min.css">
 	<link rel=stylesheet href="css/style.css">
+	<link rel=stylesheet href="css/profile.css">
 	<script type="text/javascript" src="bower_components/push.js/push.min.js"></script>
 	<script src="js/insertmatch.js"></script>
 </head>
 <body>
 	<div class="container-fluid">
 		<div class="row">
+			<div class="col-md-4 col-md-offset-4 ">
+		<div class="panel widget">
+			<div class="widget-header bg-primary"></div>
+			<div class="widget-body text-center">
+				<img alt="Profile Picture" class="widget-img img-circle img-border-light" src="http://bootdey.com/img/Content/avatar/avatar1.png">
+				<h4 class="mar-no">John Doe</h4>
+				<p class="text-muted mar-btm">Administrator</p>
+
+				<div class="pad-ver">
+					<button class="btn btn-primary">Follow</button>
+					<button class="btn btn-success">Message</button>
+				</div>
+			</div>
+		</div>
+	</div>
+		</div>
+
+		<div class="row">
 			<div class="col-lg-12">
-          <table class="table table-striped">
+          <table class="table table-striped hidden">
             <thead>
               <tr>
                 <th>#</th>
@@ -31,7 +50,7 @@
               </tr>
             </thead>
             <tbody>
-              <?php 
+              <?php
               	getAllUsername ();
               ?>
             </tbody>
